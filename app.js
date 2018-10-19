413e145e4c84cecad2362eb157858e96583fb2b7
Patrick Hogan <phogan110@live.com>
2018-10-18 23:16:39 -0600

    migrated for loop into 'click' event; changed console.log to adding textContent to `p`

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var btnText = document.createTextNode('Sing!')
 btn.appendChild(btnText);
 document.body.appendChild(btn);
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     for (i = 0; i < 5; i++) {
         var friendDiv = document.createElement('div');
         document.body.appendChild(friendDiv);
@@ -21,18 +21,13 @@ btn.addEventListener('click', function() {
         var p = document.createElement('p')
         friendDiv.appendChild(p)
 
-    }
-
-    for (i = 0; i < friends.length; i++){
-        var friend = friends[i]; {
-            for (s = 99; s > 0; s--) {
-                var minusOne = s - 1;
-                if (s > 1) {
-                    console.log(s + ' lines of code in the file, ' + s + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file');
-                } else {
-                    console.log('1 line of code in the file, 1 line of code; ' + friend + ' strikes one out, clears it all out, no more lines of code in the file');
-                }
-            }
-        }
+        for (s = 99; s > 0; s--) {
+            var minusOne = s - 1;
+            if (s > 1) {
+                divClass[i].lastChild.textContent += ' ' + s + ' lines of code in the file, ' + s + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file';
+            } else {
+                divClass[i].lastChild.textContent += ' 1 line of code in the file, 1 line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file';
+            };
+        };
     };
 });
