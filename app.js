fd2d8d689c8eb2b583ed32d65fbd9533d802382c
Patrick Hogan <phogan110@live.com>
2018-10-19 15:36:08 -0600

    moved `p` var and creation to the second loop in both statements

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ btn.addEventListener('click', function () {
         var divClass = document.getElementsByClassName('friend');
         divClass[i].firstChild.textContent = friends[i];//adds friends based on divClass array
 
-        var p = document.createElement('p')
-        friendDiv.appendChild(p)//adds p to `friendDiv`s
-
         for (s = 99; s > 0; s--) {
             var minusOne = s - 1;
             if (s > 1) {
+                var p = document.createElement('p')
+                friendDiv.appendChild(p)//adds p to `friendDiv`s
                 divClass[i].lastChild.textContent += s + ' lines of code in the file, ' + s + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file ';
             } else {
+                var p = document.createElement('p')
+                friendDiv.appendChild(p)//adds p to `friendDiv`s
                 divClass[i].lastChild.textContent += ' 1 line of code in the file, 1 line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file';
             };
         };//adds textContent to `p`s
