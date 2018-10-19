665d5978d0d8702b1c6180def4280802cea4805f
Patrick Hogan <phogan110@live.com>
2018-10-18 21:25:50 -0600

    pasted for loop into 'click' event for `btn`

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ btn.appendChild(btnText);
 document.body.appendChild(btn);
 
 btn.addEventListener('click', function() {
-    console.log("I've been clicked")
-})
-
-for (i = 0; i < friends.length; i++){
-    var friend = friends[i]; {
-        for (s = 99; s > 0; s--) {
-            var minusOne = s - 1;
-            if (s > 1) {
-                console.log(s + ' lines of code in the file, ' + s + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file');
-            } else {
-                console.log('1 line of code in the file, 1 line of code; ' + friend + ' strikes one out, clears it all out, no more lines of code in the file');
+    for (i = 0; i < friends.length; i++){
+        var friend = friends[i]; {
+            for (s = 99; s > 0; s--) {
+                var minusOne = s - 1;
+                if (s > 1) {
+                    console.log(s + ' lines of code in the file, ' + s + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file');
+                } else {
+                    console.log('1 line of code in the file, 1 line of code; ' + friend + ' strikes one out, clears it all out, no more lines of code in the file');
+                }
             }
         }
-    }
-};
+    };
+});
